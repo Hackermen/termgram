c4d42af4c93dda4e639b6fb45394534a1a4d8a0f
Andrei Regiani <andrei.cpp@gmail.com>
2017-11-18 17:16:26 +0100

    Added ncurses GUI (lib urwid), added handler of incoming and outcoming messages on GUI

--- termgram.py
+++ termgram.py
@@ -1,61 +1,76 @@
 #!/usr/bin/env python3
+import datetime
 import sys
 
+import urwid
 from telethon import TelegramClient
 from telethon.utils import get_display_name
-from telethon.tl.types import (
-    UpdateNewMessage, UpdateNewChannelMessage, UpdateShortMessage, UpdateShortChatMessage
-)
+from telethon.tl import types
 
 
 TERMGRAM_VERSION = 0.1
 API_ID = 
 API_HASH = ''
 
-client = TelegramClient('termgram', API_ID, API_HASH, update_workers=1)
+telegram = TelegramClient('termgram', API_ID, API_HASH, update_workers=1)
 current_chatroom = None  # User or Group you're chatting
+
+input_field = urwid.Edit('>: ')
+messages_field = urwid.Text('')
+mainframe = None  # urwid window
+mainloop = None  # urwid loop
 
 
 def init():
-    client.add_update_handler(update_handler)
+    telegram.add_update_handler(update_handler)
+
+    global mainframe
+    mainframe = urwid.LineBox(
+        urwid.Pile([
+            ('weight', 70, urwid.Filler(messages_field)),
+            ('fixed', 1, urwid.Filler(input_field)),
+        ]),
+    )
+    global mainloop
+    mainloop = urwid.MainLoop(mainframe, unhandled_input=input_handler)
 
 
 def welcome():
     t = '''
-         _ 
+         _
         | |
-        | |_ ___ _ __ _ __ ___   __ _ _ __ __ _ _ __ ___  
-        | __/ _ \ '__| '_ ` _ \ / _` | '__/ _` | '_ ` _ \ 
+        | |_ ___ _ __ _ __ ___   __ _ _ __ __ _ _ __ ___
+        | __/ _ \ '__| '_ ` _ \ / _` | '__/ _` | '_ ` _ \\
         | ||  __/ |  | | | | | | (_| | | | (_| | | | | | |
          \__\___|_|  |_| |_| |_|\__, |_|  \__,_|_| |_| |_|
                                  __/ |
-                                |___/   v{}              
+                                |___/   v{}
         '''
     print(t.format(TERMGRAM_VERSION))
 
 
 def login():
-    client.connect()
-    while not client.is_user_authorized():
+    telegram.connect()
+    while not telegram.is_user_authorized():
         try:
             phone = input("Phone number: ")
-            client.sign_in(phone=phone)
+            telegram.sign_in(phone=phone)
             code = input("Activation code: ")
-            client.sign_in(code=code)
-            if not client.is_user_authorized():
+            telegram.sign_in(code=code)
+            if not telegram.is_user_authorized():
                 print("Failed to authenticate. Try again.\n")
         except KeyboardInterrupt:
             sys.exit(0)
 
 
 def loop():
+    global current_chatroom
     while True:
         try:
             if not current_chatroom:
                 select_chatroom()
             active_chatroom()
         except KeyboardInterrupt:
-            global current_chatroom
             current_chatroom = None
             print('\n')
             select_chatroom()
@@ -65,7 +80,7 @@ def select_chatroom():
     try:
         options = {}
         option_count = 0
-        dialogs, entities = client.get_dialogs(15)
+        dialogs, entities = telegram.get_dialogs(15)
         for entity in entities:
             label = get_display_name(entity)
             options[option_count] = entity
@@ -79,31 +94,46 @@ def select_chatroom():
 
 
 def active_chatroom():
-    while True:
-        msg = input('')
-        client.send_message(current_chatroom, msg)
+    mainloop.run()
 
 
 def display_message(date, sender_id, message):
     date = date.strftime("%H:%M")
-    sender_name = get_display_name(client.get_entity(sender_id))
+    sender_name = get_display_name(telegram.get_entity(sender_id))
     if not message:
-        message = '{multimedia}'
-    print("[{}] {}: {}".format(date, sender_name, message))
+        message = '{multimedia ¯\_(ツ)_/¯}'
+    message = "[{}] {}: {}".format(date, sender_name, message)
+    messages_field.set_text(messages_field.text + '\n' + message)
+    mainloop.draw_screen()
 
 
 def update_handler(update):
-    if isinstance(update, (UpdateNewMessage, UpdateNewChannelMessage)):
+    if isinstance(update, (types.UpdateNewMessage, types.UpdateNewChannelMessage)):
         if update.message.from_id == current_chatroom.id:
             display_message(update.message.date, update.message.from_id, update.message.message)
 
-    elif isinstance(update, UpdateShortMessage):
+    elif isinstance(update, types.UpdateShortMessage):
         if update.user_id == current_chatroom.id:
             display_message(update.date, update.user_id, update.message)
 
-    elif isinstance(update, UpdateShortChatMessage):
+    elif isinstance(update, types.UpdateShortChatMessage):
         if update.chat_id == current_chatroom.id:
             display_message(update.date, update.from_id, update.message)
+
+
+def input_handler(key):
+    if key == 'enter':
+        msg = input_field.get_edit_text()
+        telegram.send_message(current_chatroom, msg)
+        display_message(datetime.datetime.now(), telegram.get_me(), msg)
+        input_field.set_edit_text('')  # clear
+
+    elif key == 'esc':
+        # @TODO: select another chatroom?
+        pass
+
+    elif key == 'ctrl l':
+        messages_field.set_text('')  # clear all messages
 
 
 def main():
