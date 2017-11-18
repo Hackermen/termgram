3fa7ec880a275534c96e6a14317e3dcc22cd93e9
Andrei Regiani <andrei.cpp@gmail.com>
2017-11-18 01:21:08 +0100

    Improved main loop, handles ^C to change chatroom or exit, comments and better variable naming

--- termgram.py
+++ termgram.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import sys
+
 from telethon import TelegramClient
 from telethon.utils import get_display_name
 from telethon.tl.types import UpdateNewMessage
@@ -9,7 +11,7 @@ API_ID =
 API_HASH = ''
 
 client = TelegramClient('termgram', API_ID, API_HASH, update_workers=1)
-selected_chat = None
+current_chatroom = None  # User or Group you're chatting
 
 
 def init():
@@ -41,42 +43,56 @@ def login():
             print("Failed to authenticate. Try again.\n")
 
 
+def loop():
+    while True:
+        try:
+            if not current_chatroom:
+                select_chatroom()
+            active_chatroom()
+        except KeyboardInterrupt:
+            print('\n')
+            select_chatroom()
+
+
 def select_chatroom():
-    options = {}
-    option_count = 0
-    dialogs, entities = client.get_dialogs(10)
-    for entity in entities:
-        label = get_display_name(entity)
-        options[option_count] = entity
-        print("{:>3}: {}".format(option_count, label))
-        option_count += 1
-    answer = int(input("\nSelect chatroom: "))
-    global selected_chat
-    selected_chat = options[answer]
+    try:
+        options = {}
+        option_count = 0
+        dialogs, entities = client.get_dialogs(10)
+        for entity in entities:
+            label = get_display_name(entity)
+            options[option_count] = entity
+            print("{:>3}: {}".format(option_count, label))
+            option_count += 1
+        answer = int(input("\nSelect chatroom: "))
+        global current_chatroom
+        current_chatroom = options[answer]
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit(0)
 
 
-def open_chatroom():
+def active_chatroom():
     while True:
         input('')
 
 
 def update_handler(update):
     if isinstance(update, UpdateNewMessage):
-        if update.message.from_id == selected_chat.id:
+        if update.message.from_id == current_chatroom.id:
             date = str(update.message.date).split()[1][:-3]  # HH:MM
-            from_label = get_display_name(client.get_entity(update.message.from_id))
+            sender = get_display_name(client.get_entity(update.message.from_id))
             message = update.message.message
             if not message:
-                message = '{media}'
-            print("[{}] {}: {}".format(date, from_label, message))
+                message = '{multimedia}'
+            print("[{}] {}: {}".format(date, sender, message))
 
 
 def main():
     init()
     welcome()
     login()
-    select_chatroom()
-    open_chatroom()
+    loop()
 
 
 if __name__ == '__main__':
