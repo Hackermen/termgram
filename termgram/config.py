a89f3d1c726931e34318870703b4901e9ddfbfa9
Andrei Regiani <andrei.cpp@gmail.com>
2017-11-23 02:24:54 +0100

    General refactor: improved imports, global variables (type hinting), created config.py, improved main loop logic

--- termgram/config.py
+++ termgram/config.py
@@ -1,7 +1,28 @@
-APP_VERSION = 0.1
-APP_NAME = 'termgram'
+from os import makedirs
+from os.path import expanduser
 
-TELEGRAM_API_ID = 
-TELEGRAM_API_HASH = ''
+
+TELEGRAM_ID = 
+TELEGRAM_HASH = ''
+
+CONFIG_DIR = expanduser('~') + '/.termgram/'
+SESSION_FILE = CONFIG_DIR + 'auth'
+
+APP_VERSION = 0.1
+APP_LOGO = '''
+         _
+        | |
+        | |_ ___ _ __ _ __ ___   __ _ _ __ __ _ _ __ ___
+        | __/ _ \ '__| '_ ` _ \ / _` | '__/ _` | '_ ` _ \\
+        | ||  __/ |  | | | | | | (_| | | | (_| | | | | | |
+         \__\___|_|  |_| |_| |_|\__, |_|  \__,_|_| |_| |_|
+                                 __/ |
+                                |___/   v{}
+        '''.format(APP_VERSION)
+
 
 TIMESTAMP_FORMAT = '%H:%M'
+
+
+# Init
+makedirs(CONFIG_DIR, exist_ok=True)
