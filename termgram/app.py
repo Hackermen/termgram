39301e3f61b9d86a8d56b5328ab10df876383221
Andrei Regiani <andrei.cpp@gmail.com>
2017-11-24 01:03:35 +0100

    GUI improvements, New messages appear on the bottom (natural order), Input handler accordingly to selected pane, Added function comments, several comestic changes

--- termgram/app.py
+++ termgram/app.py
@@ -22,13 +22,17 @@ input_field = None  # type: urwid.Edit
 
 
 def run():
+    """Entry point"""
+
     init()
     login()
-    loop()
+    live_chatroom()
     exit_program()
 
 
 def init():
+    """Initialize components"""
+
     # Telegram
     if not any([config.TELEGRAM_ID, config.TELEGRAM_HASH]):
         print("Missing Telegram API keys at config.py")
@@ -42,10 +46,12 @@ def init():
     header_text = urwid.Text('termgram')
     header_text.set_align_mode('center')
     global input_field
-    input_field = urwid.Edit('> ')
+    input_field = urwid.Edit('>>> ')
 
 
 def login():
+    """First time login"""
+
     client.connect()
     while not client.is_user_authorized():
         try:
@@ -59,11 +65,9 @@ def login():
             exit_program()
 
 
-def loop():
-    live_chatroom()
-
-
 def event_polling(update):
+    """Telegram event updates"""
+
     if isinstance(update, (types.UpdateNewMessage, types.UpdateNewChannelMessage)):
         if update.message.from_id == current_chat.id or update.message.to_id.channel_id == current_chat.id:
             display_message(update.message.date, update.message.from_id, update.message.message)
@@ -78,8 +82,28 @@ def event_polling(update):
 
 
 def input_handler(key):
-    global current_chat
+    """Handle events accordingly to the current focused widget"""
 
+    # Left Column
+    if columns.focus_col == 0:
+        # Input field
+        if mainframe.focus_part == 'footer':
+            message_input_handler(key)
+        # Message logs
+        else:
+            logs_input_handler(key)
+
+    # Right Column
+    elif columns.focus_col == 1:
+        # Chat list
+        chatlist_input_handler(key)
+
+
+def message_input_handler(key):
+    """Handle events while writing a message"""
+
+    global current_chat
+    # Send message
     if key == 'enter':
         my_message = input_field.get_edit_text()
         if my_message.strip() and current_chat:
@@ -88,11 +112,6 @@ def input_handler(key):
             input_field.set_edit_text('')  # clear input
 
     elif key == 'esc':
-        current_chat = None
-        raise urwid.ExitMainLoop()
-
-    elif key == 'ctrl l':
-        # @TODO: clear all messages
         pass
 
     elif key == 'tab':
@@ -102,9 +121,23 @@ def input_handler(key):
             mainframe.focus_part = 'footer'
 
 
+def logs_input_handler(key):
+    """Handle events while message history is in focus"""
+
+    pass
+
+
+def chatlist_input_handler(key):
+    """Handle events while chat list is in focus"""
+
+    pass
+
+
 def live_chatroom():
+    """Main loop"""
+
     global message_list
-    message_list = urwid.ListBox(urwid.SimpleFocusListWalker([]))
+    message_list = urwid.ListBox(urwid.SimpleFocusListWalker([urwid.Divider()]))
     body = urwid.LineBox(message_list)
 
     global mainframe
@@ -112,7 +145,7 @@ def live_chatroom():
     mainframe.focus_part = 'footer'
 
     global columns
-    columns = urwid.Columns([build_contact_list(), mainframe])
+    columns = urwid.Columns([mainframe, (25, build_contact_list())])
 
     global mainloop
     mainloop = urwid.MainLoop(columns, unhandled_input=input_handler)
@@ -124,24 +157,31 @@ def live_chatroom():
 
 
 def build_contact_list():
+    """Returns built widget of contact list"""
+
     body = []
     _, entities = client.get_dialogs(50)
     for entity in reversed(entities):
         label = get_display_name(entity)
+        label_max_chars = 18
+        if len(label) > label_max_chars:
+            label = label[:label_max_chars] + '…'
         button = urwid.Button(label)
         urwid.connect_signal(button, 'click', on_selected_chatroom, entity)
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
     list_conversations = urwid.ListBox(urwid.SimpleFocusListWalker(body))
-    main = urwid.Padding(list_conversations, left=7, right=7)
+    main = urwid.Padding(list_conversations, left=1, right=1)
     return main
 
 
 def on_selected_chatroom(event, entity):
+    """Chat is selected"""
+
     global current_chat
     current_chat = entity
     global header_text
     header_text.set_text(get_display_name(entity))
-    columns.set_focus_column(1)
+    columns.set_focus_column(0)
     # retrieve recent chat (history)
     total, messages, senders = client.get_message_history(entity)
     for message in messages:
@@ -149,16 +189,20 @@ def on_selected_chatroom(event, entity):
 
 
 def display_message(date, sender_id, message):
+    """Appends new message to message logs"""
+
     date = date.strftime(config.TIMESTAMP_FORMAT)
     sender_name = get_display_name(sender_id)
     if not message:
         message = '{multimedia ¯\_(ツ)_/¯}'
     message = " {} | {}: {}".format(date, sender_name, message)
-    message_list.body.insert(0, urwid.Text(message))
-    message_list.set_focus(0)
+    message_list.body.insert(-1, urwid.Text(message))
+    message_list.set_focus(len(message_list.body)-1)
     mainloop.draw_screen()
 
 
 def exit_program():
+    """Gracefully exit"""
+
     client.disconnect()
     sys.exit(0)
